1570b81332ac103b0e8ed456d2e6b1a470725e05
maralar13 <maralar13@gmail.com>
2020-12-10 12:41:01 -0500

    commit 1 --> docs_db.py creado y main.py relacionado

---
+++ main.py
@@ -0,0 +1,10 @@
+from db.docs_db import DocInDB
+from db.docs_db import get_docs, update_docs
+
+
+
+
+import datetime
+from fastapi import FastAPI
+from fastapi import HTTPException
+api = FastAPI()
