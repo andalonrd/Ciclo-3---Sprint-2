956ed0150ae2b34a68ebbb85e791c0b940d422f0
Laura Valentina Garzón Güiza <64382196+lvgarzong@users.noreply.github.com>
2020-12-10 19:28:15 -0500

    Update gestion_db.py

---
+++ db/gestion_db.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from pydantic import BaseModel
+
+class AccessInDB(BaseModel):
+  id_access: int = 0
+  username: str
+  date: datetime = datetime.now()
+  value: int
+  database_access = []
+  generator = {"id":0}
+  
+  
+def save_access(access_in_db: AccessInDB):
+  generator["id"] = generator["id"] + 1
+  access_in_db.id_access = generator["id"]
+  database_access.append(access_in_db)
+  return access_in_db
