eb2c718511443c75ea32c4cdb2a9b39e6c2abf87
jbocane6 <75183250+jbocane6@users.noreply.github.com>
2020-12-11 11:18:43 -0500

    Update gestion_models.py

--- models/gestion_models.py
+++ models/gestion_models.py
@@ -1,10 +1,13 @@
 from pydantic import BaseModel
 from datetime import date
+from datetime import datetime
 
 class GestionIn(BaseModel):
     docName: str
     expiration: date
 
 class GestionOut(BaseModel):
+    id_gestion: int
+    #date: datetime
     docName: str
     expiration: date
