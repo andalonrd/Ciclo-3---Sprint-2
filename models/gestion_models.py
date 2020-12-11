dfb70018dab6705aabbb2321345369708e3be451
Laura Valentina Garzón Güiza <64382196+lvgarzong@users.noreply.github.com>
2020-12-10 20:14:51 -0500

    Update gestion_models.py

--- models/gestion_models.py
+++ models/gestion_models.py
@@ -1,11 +1,11 @@
 from pydantic import BaseModel
 from datetime import datetime
 
-class AccessIn(BaseModel):
+class GestionIn(BaseModel):
   doc_name: str
   
-class AccessOut(BaseModel):
+class GestionOut(BaseModel):
   id_access: int
   doc_name: str
   date: datetime
-  end_date: datetime
+  expiration: datetime
