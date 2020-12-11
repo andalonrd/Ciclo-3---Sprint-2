20ab11a366ad4273dd6db6b679dca9c3174ab08d
jbocane6 <75183250+jbocane6@users.noreply.github.com>
2020-12-10 21:32:25 -0500

    Update gestion_models.py

--- models/gestion_models.py
+++ models/gestion_models.py
@@ -1,30 +1,18 @@
+from datetime import datetime
+from pydantic import BaseModel
 
-def document_log(i,user,doc_name,log):
-    if user != "" :
-        from datetime import date 
-        i += 1
-        registry = {"id": i , "date": str(date.today()), "user": user, "doc_name": doc_name }
-        log.append(registry)
-    return i    
-     
-def document_metadata(inList,doc_name):
-    for x in inList: 
-        if x['doc_name'] == doc_name :
-            return x 
+class GestionInDB(BaseModel):
+  id_gestion: int = 0
+  doc_Name: str
+  date: datetime = datetime.now()
+  expiration: datetime
 
-db = [{"doc_name": "hello.doc", "user": "kitty25", "expiry": "2020-12-30"},
- {"doc_name": "sunshine.doc", "user": "mags25", "expiry": "2020-12-25"}, 
-  {"doc_name": "bridge.doc", "user": "cutie24", "expiry": "2021-1-25"}, 
-  { "doc_name": "ice.doc", "user": "pinky27", "expiry": "2021-1-06" }
- ]  
-
-doc_i = 0
-doc_log = []
-found_doc = document_metadata(db,"sunshine.doc")
-print (found_doc)
-doc_i = document_log(doc_i, found_doc['user'], found_doc['doc_name'],doc_log)
-found_doc = document_metadata(db,"bridge.doc")
-print(found_doc)
-doc_i = document_log(doc_i, found_doc['user'], found_doc['doc_name'],doc_log)
-print(doc_log)
-
+database_gestion = []
+generator = {"id":0}
+  
+  
+def save_gestion(gestion_in_db: GestionInDB):
+  generator["id"] = generator["id"] + 1
+  gestion_in_db.id_gestion = generator["id"]
+  database_gestion.append(gestion_in_db)
+  return gestion_in_db
